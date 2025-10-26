477bf99d5c466b8364b79aee65dba09c9bee772a
michalJuniorabramczyk <michaljuniorabramczyk@gmail.com>
2025-10-26 12:42:22 +0000

    Add Movie, CinemaHall, MovieSession models and services

---
+++ services/movie.py
@@ -0,0 +1,36 @@
+from db.models import Movie
+
+
+def get_movies(genres_ids=None, actors_ids=None):
+    queryset = Movie.objects.all()
+    if genres_ids and actors_ids:
+        return queryset.filter(
+            genres__id__in=genres_ids,
+            actors__id__in=actors_ids,
+        ).distinct()
+    if genres_ids:
+        return queryset.filter(genres__id__in=genres_ids).distinct()
+    if actors_ids:
+        return queryset.filter(actors__id__in=actors_ids).distinct()
+    return queryset
+
+
+def get_movie_by_id(movie_id):
+    return Movie.objects.get(id=movie_id)
+
+
+def create_movie(
+    movie_title,
+    movie_description,
+    genres_ids=None,
+    actors_ids=None,
+):
+    movie = Movie.objects.create(
+        title=movie_title,
+        description=movie_description,
+    )
+    if genres_ids:
+        movie.genres.set(genres_ids)
+    if actors_ids:
+        movie.actors.set(actors_ids)
+    return movie
---
+++ services/movie_session.py
@@ -0,0 +1,43 @@
+from db.models import MovieSession, Movie, CinemaHall
+
+
+def create_movie_session(movie_show_time, movie_id, cinema_hall_id):
+    movie = Movie.objects.get(id=movie_id)
+    hall = CinemaHall.objects.get(id=cinema_hall_id)
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie=movie,
+        cinema_hall=hall,
+    )
+
+
+def get_movies_sessions(session_date=None):
+    if session_date:
+        return MovieSession.objects.filter(show_time__date=session_date)
+    return MovieSession.objects.all()
+
+
+def get_movie_session_by_id(movie_session_id):
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+    session_id,
+    show_time=None,
+    movie_id=None,
+    cinema_hall_id=None,
+):
+    session = MovieSession.objects.get(id=session_id)
+    if show_time:
+        session.show_time = show_time
+    if movie_id:
+        session.movie = Movie.objects.get(id=movie_id)
+    if cinema_hall_id:
+        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id):
+    session = MovieSession.objects.get(id=session_id)
+    session.delete()
